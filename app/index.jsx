cf43979e25f30bc7def638aea4f324c858d8f740
deymonster <deymonster@yandex.ru>
2024-10-23 10:46:45 +0500

    Add RootLayout, Auth, Menu

---
+++ app/index.jsx
@@ -0,0 +1,39 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native"; 
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-background">
+            <Text className="text-sm font-pblack text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-pblack text-primary">And the text in English</Text>
+
+            <Text className="text-sm font-pthin text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-pthin text-primary">And the text in English</Text>
+
+            <Text className="text-sm font-pextralight text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-pextralight text-primary">And the text in English</Text>
+
+            <Text className="text-sm font-plight text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-plight text-primary">And the text in English</Text>
+
+            <Text className="text-sm font-pregular text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-pregular text-primary">And the text in English</Text>
+
+            <Text className="text-sm font-pmedium text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-pmedium text-primary">And the text in English</Text>
+
+            <Text className="text-sm font-psemibold text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-psemibold text-primary">And the text in English</Text>
+
+            <Text className="text-sm font-pbold text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-pbold text-primary">And the text in English</Text>
+
+            <Text className="text-sm font-pextrabold text-primary">Текст приветствия на главной</Text>
+            <Text className="text-sm font-pextrabold text-primary">And the text in English</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+
+
